a1d8f88cb1f4a95d023d2b76f2696e33ebb32755
himelbikon <skhimelhaque@gmail.com>
2022-01-22 20:54:58 +0600

    get user by id and update endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,19 +1,28 @@
-import express from "express";
-import { protect } from "../middleware/authMiddleware.js";
+import express from "express"
+import { protect, admin } from "../middleware/authMiddleware.js"
 import {
   authUser,
   getUserProfile,
   registerUser,
   updateUserProfile,
-} from "../controllers/userController.js";
+  getUsers,
+  deleteUser,
+  updateUser,
+  getUserById,
+} from "../controllers/userController.js"
 
-const router = express.Router();
+const router = express.Router()
 
-router.route("/").post(registerUser);
-router.post("/login", authUser);
+router.route("/").post(registerUser).get(protect, admin, getUsers)
+router.post("/login", authUser)
 router
   .route(`/profile`)
   .get(protect, getUserProfile)
-  .put(protect, updateUserProfile);
+  .put(protect, updateUserProfile)
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 
-export default router;
+export default router
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { LinkContainer } from "react-router-bootstrap";
-import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
-import { logout } from "../actions/userActions";
+import React from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { LinkContainer } from "react-router-bootstrap"
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap"
+import { logout } from "../actions/userActions"
 
 const Header = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-  const userLogin = useSelector((state) => state.userLogin);
-  const { userInfo } = userLogin;
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
 
   const logoutHandler = () => {
-    dispatch(logout());
-  };
+    dispatch(logout())
+  }
 
   return (
     <header>
@@ -32,7 +32,11 @@ const Header = () => {
                 </Nav.Link>
               </LinkContainer>
               {userInfo ? (
-                <NavDropdown title={userInfo.name} id="username">
+                <NavDropdown
+                  className="mx-0"
+                  title={userInfo.name}
+                  id="username"
+                >
                   <LinkContainer to="/profile">
                     <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
@@ -47,12 +51,27 @@ const Header = () => {
                   </Nav.Link>
                 </LinkContainer>
               )}
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown className="mx-0" title="Admin" id="adminmenu">
+                  <LinkContainer to="/admin/userlist">
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+
+                  <LinkContainer to="/admin/productlist">
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+
+                  <LinkContainer to="/admin/orderlist">
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
